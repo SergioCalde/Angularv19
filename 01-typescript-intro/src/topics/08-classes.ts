0f8536ccfdff5a0941ecc904040f908bf8ee1313
Sergio C <sergio.calderon.dm@outlook.com>
2025-06-23 00:16:04 -0600

    Chore: Prioritize composition over inheritance

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -16,24 +16,44 @@ export class Person {
 
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+    
+//     public alterEgo: string;
+//     public age: number;
+//     public realName: string;
+
+//     constructor( 
+//         alterEgo: string, 
+//         age: number, 
+//         realName: string){
+//         super( realName, 'New York' );
+//         this.alterEgo = alterEgo;
+//         this.age = age;
+//         this.realName = realName;
+//     }
+// }
+export class Hero {
     
     public alterEgo: string;
     public age: number;
     public realName: string;
+    public person: Person;
 
     constructor( 
         alterEgo: string, 
         age: number, 
-        realName: string){
-        super( realName, 'New York' );
+        realName: string,
+        person: Person){
+
         this.alterEgo = alterEgo;
         this.age = age;
         this.realName = realName;
+
+        this.person = person;
     }
 }
 
-
-const ironman = new Hero('Ironman', 45, 'Tony');
+const person = new Person('Tony Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony', person);
 
 console.log(ironman);
