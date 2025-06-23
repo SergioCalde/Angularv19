0f11b637b2deb783f2f697fa77fd4157806b51f9
Sergio C <sergio.calderon.dm@outlook.com>
2025-06-23 00:08:47 -0600

    Chore: Review how to extend a class

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -16,5 +16,24 @@ export class Person {
 
 }
 
-const ironman = new Person('Ironman', 'New York');
+export class Hero extends Person {
+    
+    public alterEgo: string;
+    public age: number;
+    public realName: string;
+
+    constructor( 
+        alterEgo: string, 
+        age: number, 
+        realName: string){
+        super( realName, 'New York' );
+        this.alterEgo = alterEgo;
+        this.age = age;
+        this.realName = realName;
+    }
+}
+
+
+const ironman = new Hero('Ironman', 45, 'Tony');
+
 console.log(ironman);
