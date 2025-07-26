1a56c55c0736966630d770c49fab3793504890ca
Sergio C <sergio.calderon.dm@outlook.com>
2025-07-25 18:43:53 -0600

    chore: add environment template file

---
+++ 03-gifs-app/src/environments/environment.template.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Manage your gifs',
+
+  //Api Keys
+  giphyApiKey: '',
+
+  //URLS
+  giphyUrl:'https://api.giphy.com/v1',
+
+};
