dceada240f08a492177e2c4fab7f90f90490e539
Sergio C <sergio.calderon.dm@outlook.com>
2025-10-30 11:57:27 -0600

    feat: Pattern Validators

--- 06-reactive-forms-app/src/app/utils/form-utils.ts
+++ 06-reactive-forms-app/src/app/utils/form-utils.ts
@@ -1,12 +1,19 @@
 import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
 
 export class FormUtils {
+    //Regular Expressions
+    static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+    static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+    static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
     static getTextError(errors: ValidationErrors){
+        console.log( "errors", errors );
         const errorMessages: Record<string, string> = {
             required: 'This field is required',
             min: `This field must be greater than ${errors['min']?.min}`,
             minlength: `This field must be at least ${errors['minlength']?.requiredLength} characters`,
+            email: 'This field must be a valid email',
+            pattern: FormUtils.getPatternError(errors?.['pattern']),
         };
 
         // Obtiene la primera clave de error que exista
@@ -44,5 +51,17 @@ export class FormUtils {
 
         return this.getTextError(errors);
         
+    }
+
+    static getPatternError(patternError: any): string {
+        if (!patternError || !patternError['requiredPattern']) return 'Invalid format';
+
+        const pattern = patternError['requiredPattern'];
+        
+        if ( pattern === this.emailPattern ){
+            return 'Invalid email format'
+        }
+        
+        return 'Invalid format';
     }
 }
