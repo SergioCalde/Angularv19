9ec312f7fdfdf07833a94f53b71367189cd90c95
Sergio C <sergio.calderon.dm@outlook.com>
2025-11-19 14:32:55 -0600

    chore: remove unnecessary files

--- 07-lifecycle-hooks/src/app/pages/about-page/about-page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-page',
-  imports: [],
-  templateUrl: './about-page.html',
-})
-export class AboutPage { }
--- 08-maps-app/src/environments/environment..development.ts
+++
@@ -1,4 +0,0 @@
-
-    export const environment = {
-        mapboxKey: "pk.eyJ1Ijoic2NkbTk3IiwiYSI6ImNsNnp1NDA3ZDAwdzAzdm5uc2VicTMwcmUifQ.gPVuIBJpSrJwQ-atHZquzg"
-    };
